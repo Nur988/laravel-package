52c7a7d4b85c5990e1ebd58792c9366b7d3f9214
Nur988 <proteas988@gmail.com>
2023-06-27 14:05:57 +0600

    file size fixed

--- src/FileManager.php
+++ src/FileManager.php
@@ -257,7 +257,7 @@ class FileManager
             $s3->putObject([
                 'Bucket' => env('AWS_BUCKET'),
                 'Key' => $path,
-                'Body'=>$file,
+                'Body'=>$fileContents,
                 'Metadata' => $metadata
             ]);
           Storage::disk("unity")->putFileAs(
